1013bdbb833a3324604ddc88c1ca590c05ebb4ea
OliPro007 <OliPro007@gmail.com>
2015-03-18 19:05:48 -0400

    Base window created, will implement a button class.

--- FlickBaseball/Main.cpp
+++ FlickBaseball/Main.cpp
@@ -1,5 +1,23 @@
-#include "SFML\Graphics\RenderWindow.hpp"
+#include "SFML\Graphics.hpp"
+
+#define WIDTH 1280.0f
+#define HEIGHT 720.0f
+#define TITLE "Flick Baseball"
 
 int main() {
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode((int)WIDTH, (int)HEIGHT), TITLE);
+	window->setFramerateLimit(60);
+	sf::View* camera = new sf::View(sf::FloatRect(0.0f, 0.0f, WIDTH, HEIGHT));
+	window->setView(*camera);
+	
+	while(window->isOpen()) {		
+		sf::Event event;
+		while(window->pollEvent(event)) {
+			if(event.type == sf::Event::Closed) {
+				window->close();
+			}
+		}
+	}
+
 	return 0;
 }
